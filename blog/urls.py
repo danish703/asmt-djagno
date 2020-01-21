aa44aa5f5604f480405077e078d99b0a17a2feaf
Dipendra <dipu.danish@outlook.com>
2020-01-21 10:36:04 +0545

    second day

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('create/',views.create,name='blog_create')
+    path('create/',views.create,name='blog_create'),
+    path('all/',views.allnews,name="allnews"),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.remove,name='del'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render,redirect
 from .forms import BlogForm
+from .models import Blog
 # Create your views here.
+
+def remove(request,id):
+    a = Blog.objects.get(pk=id)
+    a.delete()
+    return redirect('allnews')
+
+def allnews(request):
+    all = Blog.objects.all()
+    context = {
+        'news':all
+    }
+    return render(request,'all_news.html',context)
+
+def edit(request,id):
+    b = Blog.objects.get(pk=id)
+    form = BlogForm(request.POST or None,instance=b)
+    if form.is_valid():
+        form.save()
+        return redirect('allnews')
+
+    context = {
+        'form':form,
+    }
+    return render(request,'edit.html',context)
+
 def create(request):
     if request.method=='GET':
         form = BlogForm()
